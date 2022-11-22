f16c69c086bacac2cdf704daa51574e96464ec9b
MytsV <mytsvictoria@gmail.com>
2022-11-22 15:50:56 +0200

    second dialog added

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   void _onMenuItemSelected(int value) {
-    Widget dialog = value == 0 ? FirstDialog(_changeText) : FirstDialog(_changeText);
+    Widget dialog = value == 0 ? FirstDialog(_changeText) : SecondDialog(_changeText);
     showDialog(
         barrierDismissible: false,
         context: context,
@@ -113,3 +113,55 @@ class FirstDialog extends StatelessWidget {
   }
 }
 
+class SecondDialog extends StatefulWidget {
+  final void Function(String) onAccept;
+  const SecondDialog(this.onAccept, {Key? key}) : super(key: key);
+
+  @override
+  State<SecondDialog> createState() => _SecondDialogState();
+}
+
+class _SecondDialogState extends State<SecondDialog> {
+  int _sliderValue = 1;
+
+  Widget _getContent() {
+    Widget slider = Slider(
+        value: _sliderValue.toDouble(),
+        min: 1,
+        max: 100,
+        divisions: 99,
+        onChanged: (value) {
+      setState(() {
+        _sliderValue = value.toInt();
+      });
+    });
+    Widget caption = Text(_sliderValue.toString());
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [caption, slider]
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text("Робота №2"),
+      content: _getContent(),
+      actions: [
+        TextButton(
+          child: const Text("Так"),
+          onPressed: () {
+            widget.onAccept(_sliderValue.toString());
+            Navigator.pop(context);
+          },
+        ),
+        TextButton(
+          child: const Text("Відміна"),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ],
+    );
+  }
+}
